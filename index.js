060cc2298913749188a4756951d1f9134444745e
Fernando Carrascosa <fcarrascosa@fcarrascosa.es>
2018-12-27 21:00:11 +0100

    feat: Add basic slides

---
+++ index.js
@@ -0,0 +1,5 @@
+import FcarrascosaSlider from './src/FcarrascosaSlider.js';
+import FcarrascosaSliderSlide from './src/FcarrascosaSliderSlide.js';
+
+customElements.define(FcarrascosaSlider.is, FcarrascosaSlider);
+customElements.define(FcarrascosaSliderSlide.is, FcarrascosaSliderSlide);
--- src/FcarrascosaSliderSlide.js
+++ src/FcarrascosaSliderSlide.js
@@ -1,5 +1,48 @@
-import { LitElement } from '@polymer/lit-element';
+import { LitElement, html } from '@polymer/lit-element';
+import { css } from 'lit-css';
 
+/**
+ * @customElement
+ */
 export default class FcarrascosaSliderSlide extends LitElement {
   static get is() { return 'fcarrascosa-slider-slide'; }
+
+  static get properties() {
+    return {
+      selected: {
+        type: Boolean,
+      },
+    };
+  }
+
+  shadowDomStyle() {
+    return css`
+      :host {
+        display: block;
+        height: 0;
+        opacity: 0;
+        transition: opacity 1.5s ease-in-out;
+        width: 100%;
+      }
+      
+      :host([selected="true"]) {
+        height: auto;
+        opacity: 1;
+      }
+      
+      ::slotted(img) {
+        display: block;
+        width: 100%;
+      }
+    `;
+  }
+
+  render() {
+    return html`
+      <style>
+        ${this.shadowDomStyle()};
+      </style>
+      <slot></slot>
+    `;
+  }
 }
