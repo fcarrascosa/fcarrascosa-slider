0918c4b547540de223386f03817f377f931d098b
Fernando Carrascosa <fcarrascosa@fcarrascosa.es>
2019-01-23 00:36:40 +0100

    feature: Add caption horizontal position, modified timer of slider

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,3 @@
-To be filled.
+# 0.3.0 to 0.4.0
+* Updated default sliding time from 2s to 4s
+* **Added caption horizontal position, can be right, left and center(*default*)**
--- src/FcarrascosaSlider.js
+++ src/FcarrascosaSlider.js
@@ -34,7 +34,7 @@ export default class FcarrascosaSlider extends LitElement {
        */
       time: {
         type: Number,
-        value: 2,
+        value: 4,
       },
 
       interval: {
@@ -56,7 +56,7 @@ export default class FcarrascosaSlider extends LitElement {
       // eslint-disable-next-line no-console
       console.warn('slider does not have any slides');
     } else {
-      this.time = this.time || 2;
+      this.time = this.time || 4;
       this.goToSlide(0);
       this.initSlider();
     }
