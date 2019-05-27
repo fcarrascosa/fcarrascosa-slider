7a18ce3c855f80aa2f5ac340fc96ef22313d9119
Fernando Carrascosa <fernando.carrascosa.contractor@bbva.com>
2019-05-27 17:31:12 +0200

    fix(webpack): change webpack.config.js to build and serve propperly

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,9 @@
 const path = require('path');
-const merge = require('webpack-merge');
 const createDefaultConfigs = require('@open-wc/building-webpack/modern-and-legacy-config');
 
-const configs = [createDefaultConfigs({
+module.exports = createDefaultConfigs({
   input: path.resolve(__dirname, './build/index.html'),
-})];
-
-module.exports = configs.map(config => merge(config, {
   output: {
     path: path.resolve(__dirname, 'build/dist'),
   },
-}));
+});
